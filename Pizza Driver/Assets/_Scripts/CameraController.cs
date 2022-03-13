aacf484bfcc708284d645b9c38a8a3fda585f3f9
marcusmoo <62606874+marcusmoo@users.noreply.github.com>
2022-03-12 20:30:57 -0800

    Found the old project

---
+++ Pizza Driver/Assets/_Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Player;
+    // Start is called before the first frame update
+    void LateUpdate()
+    {
+        transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, -20);
+    }
+
+    
+}
---
+++ Pizza Driver/Assets/_Scripts/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadInstructions()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("hello world");
+        Application.Quit();
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("menus");
+    }
+}
